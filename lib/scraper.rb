fea945083dd5a4a93b93eeba44f19965622eaf9c
Chris Lincoln <chris.lincoln12@gmail.com>
2019-08-08 13:47:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,7 @@ class Scraper
     binding.pry
   end
   
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title && course.title != ""
